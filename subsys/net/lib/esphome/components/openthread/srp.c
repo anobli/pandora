8d2a774c69caaf737fff41709b0b6b37a185ff1d
Alexandre Bailon <abailon@baylibre.com>
2025-12-08 10:13:19 +0100

    ESPHome: OpenThread: SRP: Add TXT records
    ESPHome integration in HomeAssistant relies on TXT records
    to discover automatically a ESPHome device.
    This adds the expected TXT records and allow HomeAssistant
    to discover the device automatically.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

--- subsys/net/lib/esphome/components/openthread/srp.c
+++ subsys/net/lib/esphome/components/openthread/srp.c
@@ -9,12 +9,15 @@
 LOG_MODULE_DECLARE(ESPHomeOT);
 
 #include <esphome/esphome.h>
+#include <esphome/components/api.h>
 
 #include <openthread/link.h>
 #include "srp.h"
 
-static const char *SRP_SERVICE_NAME = "_esphomelib._tcp";
+#define API_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(nabucasa_esphome_api)
 
+static const char *SRP_SERVICE_NAME = "_esphomelib._tcp";
+static const char friendly_name[] = DT_PROP_OR(DT_PARENT(API_NODE), friendly_name, "");
 static bool ot_srp_init_done = false;
 
 void ot_srp_callback(otError aError, const otSrpClientHostInfo *aHostInfo,
@@ -26,6 +29,14 @@ void ot_srp_callback(otError aError, const otSrpClientHostInfo *aHostInfo,
 	}
 	LOG_INF("SRP update registered");
 }
+
+static char mac[MAC_ADDRESS_LEN];
+otDnsTxtEntry txtEntries[] = {
+	{.mKey = "version", .mValue = (const uint8_t *)"1.0", .mValueLength = 3},
+	{.mKey = "board", .mValue = (const uint8_t *)CONFIG_BOARD, .mValueLength = sizeof(CONFIG_BOARD)},
+	{.mKey = "mac", .mValue = (const uint8_t *)mac, .mValueLength = sizeof(MAC_ADDRESS_LEN)},
+	{.mKey = "friendly_name", .mValue = (const uint8_t *)friendly_name, .mValueLength = sizeof(friendly_name)},
+};
 
 int ot_srp_init(const struct device *dev)
 {
@@ -53,8 +64,7 @@ int ot_srp_init(const struct device *dev)
 
 	otSrpClientSetCallback(ot, ot_srp_callback, NULL);
 	host_name = otSrpClientBuffersGetHostNameString(ot, &size);
-	size = MIN(size, strlen(cfg->name) + 1);
-	memcpy(host_name, cfg->name, size);
+	get_unique_device_name(cfg->name, host_name, size);
 
 	error = otSrpClientSetHostName(ot, host_name);
 	if (error != OT_ERROR_NONE) {
@@ -83,6 +93,13 @@ int ot_srp_init(const struct device *dev)
 	size = MIN(size, strlen(SRP_SERVICE_NAME) + 1);
 	memcpy(service_name, SRP_SERVICE_NAME, size);
 
+	get_mac_address_string(mac, MAC_ADDRESS_LEN);
+	for (int i = 0; i < ARRAY_SIZE(txtEntries); i++) {
+		txtEntries[i].mValueLength = strlen(txtEntries[i].mValue);
+	}
+	entry->mService.mNumTxtEntries = ARRAY_SIZE(txtEntries);
+	entry->mService.mTxtEntries = txtEntries;
+
 	error = otSrpClientAddService(ot, &entry->mService);
 	if (error != OT_ERROR_NONE) {
 		LOG_ERR("Failed to register SRP service: %s", otThreadErrorToString(error));
@@ -92,6 +109,8 @@ int ot_srp_init(const struct device *dev)
 	otSrpClientEnableAutoStartMode(ot, NULL, NULL);
 
 	ot_srp_init_done = true;
+
+	LOG_INF("SRP client initialized");
 
 	return 0;
 }
