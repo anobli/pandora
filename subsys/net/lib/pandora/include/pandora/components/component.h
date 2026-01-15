4a3d500c3e1804a411d23784a83122f0d324d95f
Alexandre Bailon <abailon@baylibre.com>
2026-01-15 22:08:09 +0100

    pandora: component: Add an iterable section for components
    This adds an interable section for components.
    This allows listing and filtering pandora components by type.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

---
+++ subsys/net/lib/pandora/include/pandora/components/component.h
@@ -0,0 +1,26 @@
+#ifndef PANDORA_COMPONENT
+#define PANDORA_COMPONENT
+
+#include <zephyr/device.h>
+#include <zephyr/sys/iterable_sections.h>
+
+#define PANDORA_COMPONENT_UNDEFINED 0
+
+struct pandora_component_item {
+	const struct device *dev;
+	int type;
+};
+
+#define DEFINE_PANDORA_COMPONENT(name, _dev, _type)                                                \
+	STRUCT_SECTION_ITERABLE(pandora_component_item, _CONCAT(pandora_component_, name)) = {     \
+		.dev = _dev,                                                                       \
+		.type = _type,                                                                     \
+	}
+
+#define DT_DEFINE_PANDORA_COMPONENT(_node, _type)                                                  \
+	DEFINE_PANDORA_COMPONENT(DEVICE_DT_NAME_GET(_node), DEVICE_DT_GET(_node), _type)
+
+#define DT_INST_DEFINE_PANDORA_COMPONENT(_inst, _type)                                             \
+	DT_DEFINE_PANDORA_COMPONENT(DT_DRV_INST(_inst), _type)
+
+#endif /* PANDORA_COMPONENT */
