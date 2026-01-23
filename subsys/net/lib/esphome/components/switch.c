b9cd0cef92379cc96d0c157b62989f7a93d0f110
Alexandre Bailon <abailon@baylibre.com>
2026-01-23 14:08:36 +0100

    eshpome: switch: Add callback to publish state on update
    This adds a callback to notify HomeAssistant when the switch state change.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

--- subsys/net/lib/esphome/components/switch.c
+++ subsys/net/lib/esphome/components/switch.c
@@ -31,9 +31,34 @@ static int esphome_switch_publish_state(const struct device *api_dev, struct esp
 	return SwitchStateResponseWrite(api_dev, &response);
 }
 
+PANDORA_CB(switch, on_state, esphome_switch_on_state, NULL)
+{
+	struct esphome_entity *entity = find_entity_by_dev(dev);
+	if (entity) {
+		SwitchStateResponse response = SWITCH_STATE_RESPONSE__INIT;
+
+		response.state = state;
+		response.key = entity->data->key;
+
+		SwitchStateResponseWrite(ESPHOME_API_DEVICE, &response);
+	}
+}
+
 #define DEFINE_ESPHOME_SWITCH(_num, _component)                                                    \
 	DEFINE_ESPHOME_ENTITY(_num, esphome_switch_##_component##_num, "switch." #_component,      \
 			      esphome_switch_list_entity, esphome_switch_publish_state);
 
 DT_FOREACH_STATUS_OKAY_VARGS(pandora_switch_gpio, DEFINE_ESPHOME_SWITCH, gpio);
 DT_FOREACH_STATUS_OKAY_VARGS(pandora_switch_hbridge, DEFINE_ESPHOME_SWITCH, hbridge);
+
+/* This exposes switch to enable or disable pandora pipelines */
+DT_FOREACH_STATUS_OKAY_VARGS(pandora_pipeline, DEFINE_ESPHOME_SWITCH, pipeline);
+
+static int esphome_switch_init(void)
+{
+	pandora_cb_enable(switch, on_state, &esphome_switch_on_state);
+
+	return 0;
+}
+
+SYS_INIT(esphome_switch_init, APPLICATION, 99);
